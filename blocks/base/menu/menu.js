9df6fbf3da5fba45036a20949e1262b816720e56
Nikita Frolov <nikita@frolov.fi>
2014-10-07 14:21:12 +0300

    Added base blocks

---
+++ blocks/base/menu/menu.js
@@ -0,0 +1,21 @@
+(function (document) {
+
+  var menu = document.querySelector('.menu');
+
+  if (!menu) {
+    return;
+  }
+
+  var toggle = document.createElement('button');
+
+  toggle.type = 'button';
+  toggle.className = 'menu__toggle';
+  toggle.textContent = menu.getAttribute('aria-label');
+
+  toggle.addEventListener('click', function () {
+    menu.classList.toggle('menu_active');
+  }, false);
+
+  menu.insertBefore(toggle, menu.firstChild);
+
+})(document);
